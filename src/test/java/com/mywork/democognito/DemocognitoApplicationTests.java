fd08084fc9e922251af929d67f088597be46c5b0
pramodpatwawork <pramod_patwa2003@yahoo.com>
2021-08-11 15:57:23 +0530

    first commit

---
+++ src/test/java/com/mywork/democognito/DemocognitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mywork.democognito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemocognitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
